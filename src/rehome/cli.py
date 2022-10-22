4b067101c066f764a19f072c19aa2439d3963d9c
Louis Maddox <louismmx@gmail.com>
2022-10-22 13:10:59 +0100

    Demo of CLI warning bug

--- src/rehome/cli.py
+++ src/rehome/cli.py
@@ -1,9 +1,13 @@
 import defopt
+import warnings
 
 from .new_home import Rehome
 
 __all__ = ["cli"]
 
 def cli():
-    home = defopt.run(Rehome)
-    breakpoint()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        home = defopt.run(Rehome, strict_kwonly=True)
+        if home.debug:
+            breakpoint()
--- src/rehome/new_home.py
+++ src/rehome/new_home.py
@@ -1,7 +1,19 @@
-from dataclasses import dataclass, field
+from dataclasses import KW_ONLY, dataclass, field
+from pathlib import Path
 
 __all__ = ["Rehome"]
 
+
 @dataclass
 class Rehome:
+    path: Path
+    _: KW_ONLY
     colocate: list[str] = field(default_factory=list)
+    debug: bool = False
+
+    @property
+    def name_groups(self) -> list[list[str]]:
+        """
+        Names which should be kept together
+        """
+        return [c.split() for c in self.colocate]
